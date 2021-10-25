bb9a996db7799e023f5624845dceb10b564b647b
Maciej Tomaszewicz <1994tomaszewicz@gmail.com>
2021-10-25 20:20:46 +0200

    Local storage storing table position

--- js/App.js
+++ js/App.js
@@ -6,29 +6,84 @@ class App {
 
         const addBtn = document.querySelector(".add_table_btn");
 
-        addBtn.addEventListener("click", this.addTable);
-        this.displayFromLocalStorage();
+        addBtn.addEventListener("click", e => this.addTable(e));
+
+        this.arrayOfTables = [];
 
     }
-    addTable() {
+    addTable(e) {
 
         const table = new Table();
 
-    }
-    displayFromLocalStorage() {
-        for (let i = 0; i < localStorage.length; i++) {
-            const key = localStorage.key(i);
+        this.arrayOfTables.push(table);
 
-            const serializedTable = localStorage.getItem(key);
-            const deserializedTable = JSON.parse(serializedTable);
-            console.log(deserializedTable)
-        }
+        this.serializeArrayOfTables(this.arrayOfTables)
+
     }
-    displayTables({table}) {
-        
+    serializeArrayOfTables(tables) {
+
+        const serializedArrayOfTable = tables.map(table => {
+            const container = {};
+
+            container[table.id] = JSON.stringify(table)
+
+            return container;
+
+
+        })
+        this.addTablesToLocal(serializedArrayOfTable);
+
     }
+    addTablesToLocal(array) {
+        array.forEach(table => {
+
+            const key = Object.keys(table)[0];
+            const value = table[key]
+
+            localStorage.setItem(key, value)
+
+            // localStorage.setItem(table.[0], table.serializedTable)
+        })
+    }
+    displayTables() {
+
+
+
+    }
+    // displayFromLocalStorage() {
+    //     for (let i = 0; i < localStorage.length; i++) {
+    //         const key = localStorage.key(i);
+
+    //         const serializedTable = localStorage.getItem(key);
+    //         const deserializedTable = JSON.parse(serializedTable);
+    //         console.log(deserializedTable)
+    //     }
+    // }
+    // displayTables({table}) {
+    //     const currentTable = `<div class="inner_div"><button class="add_person_btn">Add person</button></div>`
+    // }
 
 }
 
 const app = new App()
 
+window.onload = function () {
+
+    if (localStorage.length) {
+        for (let i = 0; i < localStorage.length; i++) {
+         
+            const key = localStorage.key(i)
+
+            const value = JSON.parse(localStorage.getItem(key))
+        
+
+            // const table = `<div class="table" style="left: ${value.leftPx}px; top:  ${value.topPx}px;"><div class="inner_div"><button class="add_person_btn">Add person</button></div></div>`
+
+            // document.body.insertAdjacentHTML("beforeend", table)
+            
+            const table = new Table(value.leftPx, value.topPx, value.id)
+           
+
+        }
+    }
+}
--- js/Table.js
+++ js/Table.js
@@ -2,13 +2,18 @@ import { Person } from "./Person.js";
 import { LocalInterface } from "./LocalInterface.js";
 
 export class Table {
-    constructor() {
-      
+    constructor(leftPx, topPx, id) {
+        this.leftPx = leftPx;
+        this.topPx = topPx;
+
+        id ? this.id = id
+            : this.id = Date.now();
+
         this.isDragable = false;
-        this.table = this.#createTable()
+        this.table = this.#createTable(this.leftPx, this.topPx);
 
         this.table.addEventListener("dblclick", e => this.#getInTable(e))
-      
+
         this.isHidden = true;
 
         this.table.addEventListener("mousedown", e => this.#mouseDown(e))
@@ -19,87 +24,87 @@ export class Table {
         this.insertDivX
         this.insertDivY
 
-        this.leftPx;
-        this.topPx;
+
+
+
         this.table.querySelector(".add_person_btn").addEventListener("click", e => this.#addPerson(e),)
-        
+
         this.innerDiv = this.table.querySelector(".inner_div");
-        this.id = Date.now();
+
+
+
         // this.addTableToLocal = () => {
         //     const serializedTable = JSON.stringify(this)
         //     localStorage.setItem(this.id, serializedTable)
         // }
         // console.log(this.addTableToLocal)
-        this.#addTableToLocal()
-     
-        // addTableToLocal(this)
-        
+        // this.#addTableToLocal()
+
+        // addTableToLocal(this)        
     }
-    #addTableToLocal() {
-        const serializedTable = JSON.stringify(this)
-        localStorage.setItem(this.id, serializedTable)
-    }
-    #setTableInLocal() {
-        const serializedTable = JSON.stringify(this)
-        localStorage.setItem(this.id, serializedTable)
-    }
-    // displayFromLocalStorage() {
-    //     const serializedTable = localStorage.getItem(this.id);
-    //     const deserializedTable = JSON.parse(serializedTable)
-    //     console.log(deserializedTable)
+    // #addTableToLocal() {
+    //     const serializedTable = JSON.stringify(this)
+    //     localStorage.setItem(this.id, serializedTable)
     // }
-    #createTable() {
+    // #setTableInLocal() {
+    //     const serializedTable = JSON.stringify(this)
+    //     localStorage.setItem(this.id, serializedTable)
+    // }
 
-        const table = document.createElement('div')
-        table.innerHTML = `<div class="inner_div"><button class="add_person_btn">Add person</button></div>`
+    #createTable(leftPx, topPx) {
         
 
-        table.classList.add("table");
-        document.body.appendChild(table);
-        
+        const tableString = `<div class="table" style="left: ${leftPx}px; top: ${topPx}px;"><div class="inner_div"><button class="add_person_btn">Add person</button></div></div>`
+        // table.innerHTML = `<div class="inner_div"><button class="add_person_btn">Add person</button></div>`
+        const table = new DOMParser().parseFromString(tableString, 'text/html').body.firstElementChild;
+        // document.body.insertAdjacentHTML("beforeend", table)
+
+        document.body.appendChild(table)
+
+
         return table;
     }
 
     #addPerson() {
-        
-       
-        const person = new Person();      
-        this.innerDiv.appendChild(person.createPerson());   
+
+
+        const person = new Person();
+        this.innerDiv.appendChild(person.createPerson());
 
     }
 
     #mouseMove(e) {
-               
+
         if (this.isDragable && this.isHidden) {
 
-        this.leftPx = e.clientX - this.insertDivX;
-        this.topPx = e.clientY - this.insertDivY;
-        this.#setTableInLocal()
-                   
-        this.table.style.left = `${this.leftPx}px`;
-        this.table.style.top = `${this.topPx}px`;
+            this.leftPx = e.clientX - this.insertDivX;
+            this.topPx = e.clientY - this.insertDivY;
+
+
+            this.table.style.left = `${this.leftPx}px`;
+            this.table.style.top = `${this.topPx}px`;
 
         }
-        
-    }   
+
+    }
 
     #getInTable(e) {
-        
 
-      
-        
-        if (this.isHidden) {   
+
+
+
+        if (this.isHidden) {
             this.innerDiv.style.display = "block";
-          
-            
+
+
             this.leftPx = this.table.style.left;
-            this.topPx = this.table.style.top;            
+            this.topPx = this.table.style.top;
 
             this.table.style.left = `0`;
             this.table.style.top = `0`;
 
             this.isHidden = !this.isHidden;
-            this.table.classList.add("showtable");          
+            this.table.classList.add("showtable");
 
         } else {
             this.innerDiv.style.display = "none";
@@ -111,14 +116,22 @@ export class Table {
     }
 
     #mouseDown(e) {
-     
+
         this.isDragable = !this.isDragable;
         this.insertDivX = e.offsetX;
         this.insertDivY = e.offsetY;
-       
+
     }
     #mouseUp() {
+
+        this.#addThisTableToLocal()
         this.isDragable = !this.isDragable;
+
+    }
+    #addThisTableToLocal() {
+        
+        localStorage.setItem(this.id, JSON.stringify(this));
+        
     }
 
 }
